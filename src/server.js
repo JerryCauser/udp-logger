21beb4ca666ff6f47c850d8726d1dc71e2d93c35
Artem Abzanov <jerrycauser@gmail.com>
2022-05-18 10:45:59 +0300

    fix: server error on end fix: add warning listener

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import { EventEmitter } from 'node:events'
 import UDPLoggerSocket from './socket.js'
 import UDPLoggerWriter from './writer.js'
+import { DEFAULT_PORT } from './constants.js'
 
 /**
  * @param {UDPLoggerSocketOptions & UDPLoggerWriterOptions} [options={}]
@@ -15,24 +16,27 @@ class UDPLoggerServer extends EventEmitter {
   constructor (options) {
     super(options)
 
-    options.fileName ??= `udp-port-${options.port}.log`
+    options.fileName ??= `udp-port-${options.port || DEFAULT_PORT}.log`
 
     this.#options = options
   }
 
-  start () {
+  async start () {
     this.socket = new UDPLoggerSocket(this.#options)
     this.writer = new UDPLoggerWriter(this.#options)
 
     this.socket.pipe(this.writer)
 
     this.socket.on('error', this.handleError)
+    this.socket.on('warning', this.handleError)
     this.writer.on('error', this.handleError)
 
-    Promise.all([
+    await Promise.all([
       EventEmitter.once(this.socket, 'socket:ready'),
       EventEmitter.once(this.writer, 'writer:ready')
-    ]).then(() => this.emit('ready'))
+    ])
+
+    this.emit('ready')
 
     return this
   }
@@ -48,8 +52,7 @@ class UDPLoggerServer extends EventEmitter {
     this.socket.off('error', this.handleError)
     this.writer.off('error', this.handleError)
 
-    this.socket.destroy()
-    this.writer.destroy()
+    this.socket.push(null)
 
     await Promise.all([
       EventEmitter.once(this.socket, 'close'),
